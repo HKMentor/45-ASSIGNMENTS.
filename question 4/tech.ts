fe2bbf7dec6525a087105e9ad41190029edb2bfd
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-03 01:28:27 +0500

    Assignment4

---
+++ question 4/tech.ts
@@ -0,0 +1,14 @@
+//Assignment 4 //Assignment 45
+
+//4 famous qoute;find a quote from a famousperson you admire. 
+// print the quote and the name of its author. your output
+//shoul look something like the folowing, including the quotation marks;
+//albert einstein once said, 'a person who never made a mistake never tried anything new.'
+
+console.log("Whinston Churchill once said,'Success consist of going from failure to failure without loos of enthusiam.'");
+
+console.log("Whinston Churchill once said,'Never ,Never,Never give up");
+
+console.log("Whinston Churchill once said,'History will be kind to me for I intend to write it.'")
+
+console.log("Whinston Churchill once said, 'We make a living by what we get, but we make a life by whst we give.'")
