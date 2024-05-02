d4192b8665514b8226d88938f271f889e50a5d04
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-03 01:24:51 +0500

    Assignment2

---
+++ question 2/tech.js
@@ -0,0 +1,7 @@
+//45 Assignment //Assignment 2
+//personal message;store a person name in a variable,and
+//  print a message to that person your message should be
+//simple such as 'hello eric, woul you like to learnsome phython today
+
+var hello = "zainab";
+console.log("hello ".concat(hello, ", would you like to learn some phython today?"));
---
+++ question 2/tech.ts
@@ -0,0 +1,12 @@
+//45 Assignment // Assignment 2
+
+//personal message;store a person name in a variable,and
+//  print a message to that person your message should be
+ //simple such as 'hello eric, woul you like to learnsome phython today
+
+
+let hello : string = "zainab";
+
+ console.log(`hello ${hello}, would you like to learn some phython today?`)
+
+
