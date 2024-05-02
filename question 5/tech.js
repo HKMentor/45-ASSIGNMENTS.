0ee20746e734a24e788d513bc483ca4065876faa
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-03 01:29:47 +0500

    Assignment 5

---
+++ question 5/tech.js
@@ -0,0 +1,11 @@
+"use strict";
+// Assignment 5 
+//famous qoute2; repeat exercise 4, 
+// but this time store the famous_person,s name in a variable called famous_name
+//then compose your meesage and store it in a new variable called messsage.print your message
+let famous_Person = 'Allama Iqbal';
+let qoute = "Khudi Ko Kar Buland Itna, k Har Taqdeer Se Phele, Khuda Bandy Se khud Puchein k Bata teri Raza kia Hai !";
+console.log(`${famous_Person} once said, ${qoute}`);
+let famous_Name = 'Allama Iqbal';
+let qoute2 = "Ishq Teri Inteha , Isqh Meri Inteha; Tu bhi Abhi Na-Tamam, Mein bhi Abhi Na-Tamam !";
+console.log(`${famous_Name} once said, ${qoute2}`);
