28e28c5d39ad6b4eb1bdad427f31292190e64a13
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-06 00:21:47 +0500

    Assignment 9

---
+++ question9/tech.js
@@ -0,0 +1,6 @@
+//Assignment 9 //ASsignment 45
+//Favorite Number: Store your favorite number in a variable. 
+//Then, using that variable,create a message that reveals your favorite number.
+// Print that message.
+var FavNum = 7;
+console.log(FavNum + " because it shows the quantity of my family members");
---
+++ question9/tech.ts
@@ -0,0 +1,11 @@
+//Assignment 9 //ASsignment 45
+
+//Favorite Number: Store your favorite number in a variable. 
+//Then, using that variable,create a message that reveals your favorite number.
+// Print that message.
+
+let FavNum:number = 7
+console.log(FavNum +" because it shows the quantity of my family members")
+
+
+
