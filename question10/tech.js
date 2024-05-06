39aa39b817de79756c650387807db5bf1b584b36
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-07 01:45:05 +0500

    Assignment 10

---
+++ question10/tech.js
@@ -0,0 +1,23 @@
+//Assignment 10 //Assignment 45
+//Adding Comments: Choose two of the programs you’ve written,
+// and add at least one comment to each. If you don’t have anything specific 
+//to write because your programs are too simple at this point, just add your 
+//name and the current date at the top of each program file. Then write one 
+//sentence describing what the program does.
+/*----------------------------------------------------------------------- */
+//name : 'hooria'
+//date : 2024-05-06 
+//Declare a constant varible  named FavNum and assign it the value.7,
+var FavNum = 7;
+//Print thevalue of FavNUm along with a message explaning its significance.
+console.log('FavNum:' + FavNum + "  bcz it show the quntity of my family!");
+// it my lucky number. THIS IS THE REASON.
+/*----------------------------------------------------------------*/
+// Declare a variable named name5 and assign it the string "/t hooria /n"
+var name5 = "/t hooria /n";
+//Print the string  "Name Without White space:"Follow by the value of name5.
+console.log("Name With White space:" + name5);
+// Declare a variable named name2 and assign it the string " hooria "
+var name2 = 'hooria';
+console.log("Name Without White space:" + name2);
+//complete//
