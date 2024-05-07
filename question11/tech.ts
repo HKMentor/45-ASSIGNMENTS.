fd2babe8dd48b776c993bf09577212c9d9167345
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-08 00:51:53 +0500

    Assignment 11

---
+++ question11/tech.ts
@@ -0,0 +1,12 @@
+//Assignment 11  //Assignment 45
+
+//Names: Store the names of a few of your friends in a array called names.
+// Print each person’s name by accessing each element in the list, one at a time.
+
+let myfriends =["Laiba" , "Sadia" , "Tazeen" ,"Mazia" ,"Arsala"];
+console.log(myfriends[3]);
+console.log(myfriends[1]);
+console.log(myfriends[2]);
+console.log(myfriends[0]);
+console.log(myfriends[4]);
+
