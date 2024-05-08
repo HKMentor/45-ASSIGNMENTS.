babff30ec7993b61b1bb22ab9fdc49caa83f9828
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-08 21:18:08 +0500

    Assignment 12

---
+++ question12/tech.js
@@ -0,0 +1,11 @@
+//Assignment 12 //Assignment 45
+//Greetings: Start with the array you used in Exercise 11, \
+//but instead of just printing each person’s name, print a message to them.
+// The text of each message should be the same,
+// but each message should be personalized with the person’s name..
+var friends = ["Laiba", "Sadia", "Tazeen", "Mazia", "Arsala"];
+console.log(friends[0] + ": hello,have a great day.");
+console.log(friends[1] + ": hello,have a great day.");
+console.log(friends[2] + ": hello,have a great day.");
+console.log(friends[3] + ": hello,have a great day.");
+console.log(friends[4] + ": hello,have a great day.");
