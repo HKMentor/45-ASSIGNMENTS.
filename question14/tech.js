09ceca343beaabed69eba2a3cd361cb1f59473a9
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-10 02:45:49 +0500

    Assignment 14

---
+++ question14/tech.js
@@ -0,0 +1,13 @@
+//Assignment 14 //Assignment 45
+//Guest List: If you could invite anyone, living or deceased, to dinner, 
+//who would you invite? Make a list that includes at least
+// three people you’d like to invite to dinner. 
+//Then use your list to print a message to each person, inviting them to dinner.
+var guestList = [
+    "NEHA",
+    "KANWAL",
+    "RAHEELA"
+];
+console.log(guestList[0] + " :I would be honored if you could join me for dinner.");
+console.log(guestList[1] + " :I would be honored if you could join me for dinner.");
+console.log(guestList[2] + " :I would be honored if you could join me for dinner.");
---
+++ question14/tech.ts
@@ -0,0 +1,16 @@
+//Assignment 14 //Assignment 45
+//Guest List: If you could invite anyone, living or deceased, to dinner, 
+//who would you invite? Make a list that includes at least
+// three people you’d like to invite to dinner. 
+//Then use your list to print a message to each person, inviting them to dinner.
+
+
+let guestList: string[] =
+    [
+        "NEHA",
+        "KANWAL",
+        "RAHEELA"
+    ];
+console.log(guestList[0] + " :I would be honored if you could join me for dinner.");
+console.log(guestList[1] + " :I would be honored if you could join me for dinner.");
+console.log(guestList[2] + " :I would be honored if you could join me for dinner.");
