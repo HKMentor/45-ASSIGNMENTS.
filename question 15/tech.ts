f7ef49ee3932b744aa99ccd550deaf6eee0a25ff
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-10 02:47:07 +0500

    Assignment 15

---
+++ question 15/tech.ts
@@ -0,0 +1,31 @@
+// Assignment 15  //Assignment 45........
+
+//Changing Guest List: You just heard that one of your guests can’t make the dinner, 
+//so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+//• Start with your program from Exercise 14.
+// Add a print statement at the end of your program stating the name of the guest who can’t make it.
+//• Modify your list, replacing the name of the guest who can’t make it with the name
+// of the new person you are inviting.
+//• Print a second set of invitation messages, one for each person who is still in your list.
+
+// Initial guest list
+let guestList: string[] = ["NEHA", "KANWAL", "RAHEELA"];
+guestList.forEach(guest => {
+    console.log(`${guest}: I would be honored if you could join me for dinner.`);
+});
+console.log("\nRAHEELA will not come because of some urgent work!");
+
+// Update guest list
+guestList.splice(2, 1, "Maryam", "Hania");
+console.log(guestList);
+
+// Send invitations to the updated guest list
+guestList.forEach(guest => {
+    console.log(`${guest}: I would be honored if you could join me for dinner.`);
+});
+
+console.log("The END")
+
+
+
+
