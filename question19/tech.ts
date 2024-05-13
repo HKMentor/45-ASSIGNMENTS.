ca5b122fc6a34193c8765b1f94951cef7b6e7c59
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-14 01:52:31 +0500

    Assignment 19

---
+++ question19/tech.ts
@@ -0,0 +1,32 @@
+//Assignment 19 //Assignment 45.........
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18,
+// print a message indicating the number of people you are inviting to dinner.
+let invite = ["neha", "kanwal", "hooria",];
+console.log(invite);
+invite.pop();
+console.log(invite);
+invite.push("laiba", "raheela", "fatima");
+console.log(invite);
+invite.unshift("sana");
+console.log(invite);
+invite.splice(2, + "hussain", "hassan");
+console.log(invite);
+console.log(invite[0] + ": i would like to invite you to join me at dinner.");
+console.log(invite[1] + ": i would like to invite you to join me at dinner.");
+console.log(invite[2] + ": i would like to invite you to join me at dinner.");
+console.log(invite[3] + ": i would like to invite you to join me at dinner.");
+//print a new set of invitatation
+//i found a bigger dinner table//
+console.log(invite[0] + ": i would like to invite you at dinner and i found a bigger dinner table.");
+console.log(invite[1] + ": i would like to invite you at dinner and i found a bigger dinner table.");
+console.log(invite[2] + ": i would like to invite you at dinner and i found a bigger dinner table.");;
+console.log(invite[3] + ": i would like to invite you at dinner and i found a bigger dinner table.");
+console.log(invite[4] + ": i would like to invite you at dinner and i found a bigger dinner table.");
+console.log(invite[5] + ": i would like to invite you at dinner and i found a bigger dinner table.");
+console.log(invite[6] + ": i would like to invite you at dinner and i found a bigger dinner table.");
+//indicating the number of people which i have invited
+let invitepeople = invite.push();
+console.log(invitepeople);
+
+console.log("THE END");
+
