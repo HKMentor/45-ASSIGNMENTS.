91b4d717f17a315588f46d47e56985837903ac5d
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-14 02:14:20 +0500

    Assignment 20

---
+++ question20/tech.js
@@ -0,0 +1,35 @@
+//Assignment 20 //Assignment 45.........
+//Think of something you could store in a array.
+// For example, you could make a list of mountains, rivers, countries, 
+//cities, languages, or anything else you’d like. Write a program that 
+//creates a list containing these items.
+var languages = ['Urdu', 'English', 'Chines', 'Hindi', 'Turkish'];
+var mountains = ['Mount Everest', 'k2 Mountain', 'karakaram', 'Hindu kash', 'Himalya'];
+var country = ['Pakistan', 'China', 'Africa', 'India', 'Amerca'];
+var cities = ['Kaarchi', 'Multan', 'Islamabad', 'Lahore', 'Rahim yar khan'];
+var river = ['karli jheel', 'Neelam jheel', 'Ravi', 'Indus', 'Jhelum'];
+//languages
+console.log(languages[0]);
+console.log(languages[2]);
+console.log(languages[2]);
+console.log(languages[3]);
+console.log(languages[4]);
+//mountain
+console.log(mountains[0]);
+console.log(mountains[1]);
+console.log(mountains[2]);
+console.log(mountains[3]);
+console.log(mountains[4]);
+//cities
+console.log(cities[0]);
+console.log(cities[1]);
+console.log(cities[2]);
+console.log(cities[3]);
+console.log(cities[4]);
+//river
+console.log(river[0]);
+console.log(river[1]);
+console.log(river[2]);
+console.log(river[3]);
+console.log(river[4]);
+console.log("THE END");
---
+++ question20/tech.ts
@@ -0,0 +1,50 @@
+//Assignment 20 //Assignment 45.........
+//Think of something you could store in a array.
+// For example, you could make a list of mountains, rivers, countries, 
+//cities, languages, or anything else you’d like. Write a program that 
+//creates a list containing these items.
+
+let languages:string[]=['Urdu' , 'English' , 'Chines' ,'Hindi' , 'Turkish'];
+let mountains:string[]=['Mount Everest' , 'k2 Mountain' ,'karakaram','Hindu kash' , 'Himalya'];
+let country:string[]=['Pakistan' , 'China', 'Africa' , 'India' ,'Amerca'];
+let cities:string[]=['Kaarchi' , 'Multan' ,'Islamabad' , 'Lahore' , 'Rahim yar khan' ];
+let river:string[]=['karli jheel' , 'Neelam jheel' , 'Ravi' , 'Indus', 'Jhelum'];
+
+//languages
+console.log(languages[0]);
+console.log(languages[2]);
+console.log(languages[2]);
+console.log(languages[3]);
+console.log(languages[4]);
+
+//mountain
+console.log(mountains[0]);
+console.log(mountains[1]);
+console.log(mountains[2]);
+console.log(mountains[3]);
+console.log(mountains[4]);
+ 
+ //cities
+console.log(cities[0]);
+console.log(cities[1]);
+console.log(cities[2]);
+console.log(cities[3]);
+console.log(cities[4]);
+
+//river
+console.log(river[0]);
+console.log(river[1]);
+console.log(river[2]);
+console.log(river[3]);
+console.log(river[4]);
+
+console.log("THE END")
+
+
+
+
+
+
+
+
+
