47639ddb8b8c09a2631849aa2e65b839608b82d1
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-14 23:36:35 +0500

    Assignment 22

---
+++ question22/tech.js
@@ -0,0 +1,12 @@
+//Assignment 22  //Assignment 45
+//Intentional Error: If you haven’t received an array index error
+// in one of your programs yet, try to make one happen. 
+//Change an index in one of your
+// programs to produce an index error. 
+//Make sure you correct the error before closing the program.
+//index error
+var array = [true, false];
+console.log(array);
+//correction error
+var array1 = [true, false];
+console.log(array1);
