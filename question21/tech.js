17d5a92920dceb81478907b6b9d8033775494b4d
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-14 23:33:03 +0500

    Assignment 21

---
+++ question21/tech.js
@@ -0,0 +1,13 @@
+//Assignment 21  //Assignment 45...........
+var object = {
+    Name: "Hooria",
+    Fathername: "Mohe ud din",
+    Class: 12,
+    RollNo: 12346
+};
+console.log(object);
+//Separete print........
+console.log(object.Class);
+console.log(object.Name);
+console.log(object.RollNo);
+console.log(object.Fathername);
---
+++ question21/tech.ts
@@ -0,0 +1,23 @@
+//Assignment 21  //Assignment 45...........
+
+//They think of something you could store in a TypeScript Object.
+// Write a program that creates Objects containing these items
+type details={
+Name:string
+Fathername:string
+RollNo:number
+Class:number
+};
+
+let object:details={
+    Name:"Hooria",
+     Fathername:"Mohe ud din",
+     Class:12,
+     RollNo:12346
+};
+console.log(object);
+//Separete print........
+console.log(object.Class);
+console.log(object.Name);
+console.log(object.RollNo);
+console.log(object.Fathername);
