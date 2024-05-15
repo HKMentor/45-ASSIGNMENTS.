37853acc277ab5d5fd1eec20e1f7007a90c0e359
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-16 01:04:59 +0500

    Assignment 23

---
+++ Question 23/tech.ts
@@ -0,0 +1,56 @@
+//Assignment 23 //Assignment 45............
+//Conditional Tests: Write a series of conditional tests. 
+//Print a statement describing each test and your prediction
+// for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+// 5 tests evaluate to True.
+
+console.log("HeLLO WOrlD");
+//5 TESTS  EVALUATe TRUE CONDITION!
+//Test 1
+let num:number=7;
+console.log('is num == 7?i predict true.');
+console.log(num==7)
+//Test 2
+let name1:string='hooria';
+console.log('is name == hooria?i predict true.');
+console.log(name1 == 'hooria');
+//Test 3 
+let color='blue';
+console.log('is name == blue? i predict true.');
+console.log(color == 'blue');
+//Test 4 
+let x:number = 100;
+console.log('is x == 100?i predict true.');
+console.log(x == 100);
+//Test 5
+let car= 'cultus';
+console.log('is car == cultus? i predict true.');
+console.log(car == 'cultus');
+//5 TESTS  EVALUATe FALSE CONDITION!
+//Test 6 
+let name2:string='kanwal';
+console.log('is name2 ===kanal? i predict false.');
+console.log(name2 == 'kanal' ) ;
+//Test 7
+let obj='key';
+console.log('is obj ==keys?i predict false.');
+console.log(obj == 'keys');
+//Test 8
+let str:string='hello';
+console.log('is str == hellos?i predict false.');
+console.log(str =='hellos');
+//Test 9
+let mobile:any= 'realme c21';
+console.log('is mobile1 == realme c21?i predict false.');
+console.log(mobile == 'realme c21');
+//Test 10 
+let shirt:any= 't-Shirt';
+console.log('is shirt == t-shirts? i predict false.');
+console.log(shirt == 't-shirts');
+
+console.log("THE END");
