37419c5963a72234e6c15ef55bd01a6821143006
hooria_fatima <160629586+HKMentor@users.noreply.github.com>
2024-05-23 00:08:00 +0500

    Assignment 36

---
+++ Question 36/tech.ts
@@ -0,0 +1,14 @@
+//Assignment 36   //Assignment  45...........
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message
+// that should be printed on the shirt.
+//  The function should print a sentence summarizing the size of the shirt
+// and the message printed on it. Call the function.
+
+
+function makeShirt(size: string, message: string): void {
+    console.log(`You've ordered a ${size} sized shirt with the message: "${message}".`);
+};
+
+// Calling the function
+makeShirt("Large", "Hello, World!");
+console.log("THE END");
